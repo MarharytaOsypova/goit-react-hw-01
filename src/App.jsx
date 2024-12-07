75344cd850d3dd14a870e165c0bc7a5782c4ff11
MarharytaOsypova <osipovamv03@gmail.com>
2024-12-07 18:48:29 +0200

    react1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import userData from "./userData.json";
 import Profile from "./components/Profile/Profile";
+import friends from "./friends.json";
+import FriendList from "./components/FriendList/FriendList";
+import transactions from "./transactions.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 const App = () => {
   return (
     <>
@@ -9,7 +13,9 @@ const App = () => {
         location={userData.location}
         image={userData.avatar}
         stats={userData.stats}
-      />
+          />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,21 @@
+import clsx from "clsx";
+import styles from "./FriendListItem.module.css";
+const FriendListItem = ({ avatar, name, isOnline }) => {
+    return (
+ <div className={styles.item}>
+      <img className={styles.avatar} src={avatar} alt="Avatar" width="48" />
+      <p className={styles.name}>{name}</p>
+      <p
+        className={clsx(styles.status, {
+          [styles.online]: isOnline,
+          [styles.offline]: !isOnline,
+        })}
+      >
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </div>
+
+    )
+}
+
+export default FriendListItem;
